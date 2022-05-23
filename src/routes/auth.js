101db40042683a3a71aa3d7e3547b3464a7b9972
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-23 11:38:40 -0600

    Created basic login route

---
+++ src/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body
+    if (!username || !password || username !== "testuser" || password !== "testpass") {
+        return res.status(401).json({
+            message: "Invalid credentials"
+        })
+    }
+
+    res.json({ token: "testtoken" })
+})
+
+module.exports = router
