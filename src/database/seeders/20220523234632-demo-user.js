ee36d12da0a3ad0a4f8b921be597a337230badc0
abrahamgmacias <abrahamgmacias@hotmail.com>
2022-05-23 17:54:27 -0600

    Adds single seed

---
+++ src/database/seeders/20220523234632-demo-user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [{
+      id: 1,
+      first_name: 'John',
+      second_name: 'Wayne',
+      last_name: 'Doe',
+      second_last_name: 'Moe',
+      email: 'johnwayne@gmail.com',
+      password: 'secret_password',
+      role: 'employee',
+      payment_period_id: 1,
+      business_unit_string: 'Michelada',
+      on_leave: false,
+      active: true
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
