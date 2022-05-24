94a5233edf6776a4570eeac47e86059fcc5a8d41
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-24 10:35:25 -0600

    Formatted database files to follow linter rules

--- src/database/migrations/20220523231236-create-users.js
+++ src/database/migrations/20220523231236-create-users.js
@@ -1,65 +1,65 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      first_name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      second_name: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      last_name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      second_last_name: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      role: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      privileges: {
-        type: Sequelize.JSONB,
-        allowNull: false
-      },
-      payment_period_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      business_unit_string: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      on_leave: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false
-      },
-      active: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('users');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            first_name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            second_name: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            last_name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            second_last_name: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            role: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            privileges: {
+                type: Sequelize.JSONB,
+                allowNull: false
+            },
+            payment_period_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            business_unit_string: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            on_leave: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false
+            },
+            active: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("users");
+    }
 };
--- src/database/seeders/20220523234632-demo-user.js
+++ src/database/seeders/20220523234632-demo-user.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('users', [{
-      id: 1,
-      first_name: 'John',
-      second_name: 'Wayne',
-      last_name: 'Doe',
-      second_last_name: 'Moe',
-      email: 'johnwayne@gmail.com',
-      password: 'secret_password',
-      role: 'employee',
-      privileges: {
-        privileges: [123, 456]
-      },
-      payment_period_id: 1,
-      business_unit_string: 'Michelada',
-      on_leave: false,
-      active: true
-    }], {});
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert("users", [{
+            id: 1,
+            first_name: "John",
+            second_name: "Wayne",
+            last_name: "Doe",
+            second_last_name: "Moe",
+            email: "johnwayne@gmail.com",
+            password: "secret_password",
+            role: "employee",
+            privileges: {
+                privileges: [123, 456]
+            },
+            payment_period_id: 1,
+            business_unit_string: "Michelada",
+            on_leave: false,
+            active: true
+        }], {});
+    },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('users', null, {});
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete("users", null, {});
+    }
 };
