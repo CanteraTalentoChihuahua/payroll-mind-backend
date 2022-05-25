894b62352051f06200d0ffadd2dd5f90c18b1f50
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-25 12:35:34 -0600

    Fixed issue when email doesn't exist on database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,19 +3,18 @@ const users = require("../database/models/users")(db);
 const jwt = require("../util/jwt");
 
 async function logIn(email, password) {
-    const userData = (await users.findOne({
+    const userData = await users.findOne({
         where: {email}
-    })).dataValues;
+    });
 
-    if (userData === null || userData.password !== password) {
+    if (userData === null || userData.dataValues.password !== password) {
         return {loggedIn: false, token: null};
     }
 
     const token = jwt.createSessionJWT({
-        id: userData.id,
-        role: userData.role
+        id: userData.dataValues.id,
+        role: userData.dataValues.role
     });
-    console.dir(Boolean(token));
 
     return {loggedIn: Boolean(token), token };
 }
