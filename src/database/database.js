eb7a87290fc6d26908311a9b26c2d693916fc048
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-25 11:03:46 -0600

    Readded config to connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,4 +1,5 @@
 const sqlz = require("sequelize");
+const config = require("../database/config/config.json")[process.env.NODE_ENV || "development"];
 
-const db = new sqlz(process.env.DATABASE_URL);
+const db = new sqlz(process.env.DATABASE_URL, config);
 module.exports = db;
