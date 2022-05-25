9afd32092f2b124039ba1f78692d60fd7f07da34
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-25 10:51:24 -0600

    Fixes for everyone!

--- src/database/migrations/20220524164700-create-business-units.js
+++ src/database/migrations/20220524164700-create-business-units.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('business_units', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('business_units');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("business_units", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("business_units");
+    }
 };
