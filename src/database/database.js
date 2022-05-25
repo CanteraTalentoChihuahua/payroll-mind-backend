3e4e14b52f12d0ca0bbfb6ea4fdf7668896aa4f4
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-25 10:24:57 -0600

    Fixed connection URL

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 const sqlz = require("sequelize");
-const config = require("../database/config/config.json")[process.env.NODE_ENV || "development"];
+const config = require("../database/config/config.json")[process.env.NODE_ENV || "production"];
 
 const db = new sqlz(config);
 
