148afe8a17c7f7b6c440e4c34796ef017f9631e2
el-richo <richi.cere@gmail.com>
2022-05-25 10:31:49 -0600

    PMD-64-create-catalog-migration-model-and-seeder
    Catalog model and seeder creation

---
+++ src/database/seeders/20220525160030-demo-payments_periods.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+    async up (queryInterface, Sequelize) {
+        await queryInterface.bulkInsert("payments_periods", [{
+            id: 1,
+            name: "Semanal",
+        }]);
+    },
+
+    async down (queryInterface, Sequelize) {
+        await queryInterface.bulkDelete("payments_periods", null, {});
+    }
+};
