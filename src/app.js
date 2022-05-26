74ee32a54cc69474e76fa64f02ac305e0af9ba38
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-05-26 09:30:17 -0600

    Sets base cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const trialRouter = require("./routes/trial_routes");
 const authRouter = require("./routes/auth");
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.get("/", (req, res) => {
