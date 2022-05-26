c8bad1d785b5ea1cfacee3f3ab102768e3f8acb3
el-richo <104878296+el-richo@users.noreply.github.com>
2022-05-26 11:50:08 -0600

    PMD-66-create-payments-and-payrolls-migrations-and-seeders (#16)
    Seeders and model creation for Payments and Payrolls tables
    
    Co-authored-by: opamind <105737443+opamind@users.noreply.github.com>
    Co-authored-by: Brandom Rodriguez <brandomrobor@tuta.io>
    Co-authored-by: abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
    Co-authored-by: Octavio Palacios <86976885+octavio-palaciosdev@users.noreply.github.com>

---
+++ src/database/migrations/20220526170123-create-payrolls.js
@@ -0,0 +1,40 @@
+"use strict";
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("payrolls", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            payment_date: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            payment_period_id: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            business_unit_id: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            total_amount: {
+                allowNull: false,
+                type: Sequelize.DECIMAL
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("payrolls");
+    }
+};
