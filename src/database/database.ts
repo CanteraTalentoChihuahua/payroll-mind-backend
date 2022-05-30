2d0cf8b48b9c77ea0338a468332c43738f0d7503
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-30 12:06:51 -0600

    Deleted old database configuration file

--- src/database/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-const config = require("../database/config/config.json")[process.env.NODE_ENV || "localhost"];
-import env from "dotenv";
-
-env.config();
-
-const db = new Sequelize(process.env.DATABASE_URL!, config);
-export default db;
