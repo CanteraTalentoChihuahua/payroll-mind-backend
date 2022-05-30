a9de69d29cee10c5bea4192bd654713c7dff1ea0
Brandom Rodriguez <brandomrobor@tuta.io>
2022-05-30 10:30:59 -0600

    Moved docs file inside src directory Every file used by a Typescript file MUST be inside the src directory

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 import ui from "swagger-ui-express";
-import json from "../../docs/openapi.json";
+import json from "../docs/openapi.json";
 
 router.use("/docs", ui.serve);
 router.get("/docs", ui.setup(json));
