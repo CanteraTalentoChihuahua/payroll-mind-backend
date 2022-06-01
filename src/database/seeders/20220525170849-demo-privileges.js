e0eb820e6cddd4c07b97246a9a695d05efb8e53b
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2022-06-01 09:43:28 -0600

    [PMD-55] Establish privileges for admin CRUD (#21)
    * Added admin privileges to seeder
    
    * Added roles to catalog seeders

--- src/database/seeders/20220525170849-demo-privileges.js
+++ src/database/seeders/20220525170849-demo-privileges.js
@@ -4,10 +4,20 @@ module.exports = {
     async up (queryInterface, Sequelize) {
         await queryInterface.bulkInsert("privileges", [{
             description: "Crear administradores",
-        },
-        {
+        },{
             description: "Crear unidades de negocio",
-        }]);
+        },{
+            description: "Crear usuarios"
+        },{
+            description: "Editar usuarios"
+        },{
+            description: "Borrar usuarios"
+        },{
+            description: "Generar recibos de nómina"
+        },{
+            description: "Editar datos de nómina"
+        }
+        ]);
     },
 
     async down (queryInterface, Sequelize) {
