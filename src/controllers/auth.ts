bec4ab69197968f1bbab7361b62e9bf10a836b64
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-01 10:09:18 -0600

    Adds forgot password route and controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,9 @@
+import jwt from "../util/jwt";
 import db from "../database/database";
 const users = require("../database/models/users")(db);
-import jwt from "../util/jwt";
+import { sign, verify, JsonWebTokenError, JwtPayload } from "jsonwebtoken";
+import transporter from "../config/mailer";
+import bcrypt from "bcrypt"
 
 async function logIn(email: string, password: string) {
     const userData = await users.findOne({
@@ -19,4 +22,65 @@ async function logIn(email: string, password: string) {
     return { loggedIn: Boolean(token), token };
 }
 
-export default { logIn };
+async function createURL(userId:string, purpose:string) {
+    const token = sign({ userId, purpose }, process.env.JWT_SECRET!, {
+        expiresIn: "10m",
+    });
+
+    // Set token to users 
+    await users.update({ token }, {
+        where: {
+            id: userId
+        }
+    });
+
+    return `${process.env.FRONT}/${purpose}/${token}`;
+}
+
+// Replace FRONT
+async function sendPasswordEmail(email:string) {
+    const userData = await users.findOne({
+        where: { email }
+    });
+
+    if (userData === null) {
+        return { isSuccessful: false };
+    }
+
+    const urlToken = await createURL(userData.id, "forgot");
+
+    let message = {
+        from: "Mind Group + <" + process.env.MAIL_ADDR + ">",
+        to: email,
+        subject: "Restore password",
+        text: `Click on the following link to restore your password: ${urlToken}`,
+    };
+
+    try {
+        const info = await transporter.sendMail(message);
+
+        if (info.accepted[0] === email) {
+            return { isSuccessful: true, result: info };
+        }
+
+    } catch (error) {
+        return { isSuccessful: false };
+    } 
+
+}
+
+async function invalidateToken(userId:string) {
+    try {
+        await users.update({ token: null }, {
+            where: { id: userId }
+        });
+        return true;
+
+    } catch (error) {
+        return false;
+    }
+}
+
+
+
+export { logIn, sendPasswordEmail };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import controller from "../controllers/auth";
+import {
+    logIn,
+    sendPasswordEmail
+} from "../controllers/auth";
+
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
@@ -10,7 +14,7 @@ router.post("/login", async (req, res) => {
         });
     }
 
-    const tokenData = await controller.logIn(email, password);
+    const tokenData = await logIn(email, password);
 
     if (!tokenData.loggedIn) {
         return res.status(401).json({
@@ -20,5 +24,21 @@ router.post("/login", async (req, res) => {
 
     res.json({ token: tokenData.token });
 });
+
+router.post("/forgot", async (req, res) => {
+    const { email } = req.body;
+    const emailStatus = await sendPasswordEmail(email);
+
+    console.log("Works??")
+
+    if (emailStatus!.isSuccessful === true) {
+        res.status(200).send("Email sent.");
+
+    } else {
+        res.status(500).send("Unable to send email.");
+    }
+});
+
+
 
 export default router;
