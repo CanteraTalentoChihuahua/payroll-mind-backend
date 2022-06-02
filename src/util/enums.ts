4744dee8b99b116f1798ec9df703ee5bc0e4e53e
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2022-06-02 10:47:38 -0600

    Created privileges enumeration

---
+++ src/util/enums.ts
@@ -0,0 +1,9 @@
+export enum Privileges {
+    CREATE_ADMIN = "Crear administradores",
+    CREATE_BUSINESS_UNITS = "Crear unidades de negocio",
+    CREATE_USERS = "Crear usuarios",
+    EDIT_USERS = "Editar usuarios",
+    DELETE_USERS = "Borrar usuarios",
+    CREATE_PAYROLL_RECEIPTS = "Generar recibos de nómina",
+    EDIT_PAYROLL = "Editar datos de nómina"
+}
