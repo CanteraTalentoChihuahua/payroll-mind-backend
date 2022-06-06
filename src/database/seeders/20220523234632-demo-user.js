f4c16f52dd031bb1d2749b33f41ce98babc2c3bb
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-06 10:19:06 -0600

    Creates collaborator and administrator role

--- src/database/seeders/20220523234632-demo-user.js
+++ src/database/seeders/20220523234632-demo-user.js
@@ -9,13 +9,13 @@ module.exports = {
             second_last_name: "Moe",
             email: "johnwayne@gmail.com",
             password: "secret_password",
-            role: "employee",
+            role: "superadmin",
             privileges: {
-                privileges: [123, 456]
+                privileges: [1, 2, 3, 4, 5, 9, 10]
             },
             payment_period_id: 1,
             business_unit: {
-                business_unit_ids: [123, 456]
+                business_unit_ids: [1, 2]
             },
             on_leave: false,
             active: true,
@@ -23,7 +23,48 @@ module.exports = {
             token: null,
             // Currently, bulk inserting JSON is bugged and this is a workaround
             // https://github.com/sequelize/sequelize/issues/8310
-        }], { timestamps: true },
+        },
+        {
+            first_name: "Abraham",
+            second_name: null,
+            last_name: "Gonzalez",
+            second_last_name: "Macias",
+            email: "abrahamgmacias@gmail.com",
+            password: "secret_password",
+            role: "admin",
+            privileges: {
+                privileges: [1]
+            },
+            payment_period_id: 1,
+            business_unit: {
+                business_unit_ids: [3, 4, 5, 6, 7, 8]
+            },
+            on_leave: false,
+            active: true,
+            salary: 0,
+            token: null,
+        },
+        {
+            first_name: "Elvis",
+            second_name: null,
+            last_name: "Tek",
+            second_last_name: "Tonik",
+            email: "elvistek@gmail.com",
+            password: "secret_password",
+            role: "collab",
+            privileges: {
+                privileges: []
+            },
+            payment_period_id: 1,
+            business_unit: {
+                business_unit_ids: [1]
+            },
+            on_leave: false,
+            active: true,
+            salary: 20,
+            token: null,
+        }],
+            { timestamps: true },
             {
                 privileges: { type: new Sequelize.JSON() },
                 business_unit: { type: new Sequelize.JSON() }
