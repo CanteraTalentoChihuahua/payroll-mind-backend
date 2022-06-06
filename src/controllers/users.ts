4df8274c35fb61334bc8dd96a2080cc0b7f5ce30
Brandom Rodriguez <brandomrobor@tuta.io>
2022-06-06 16:24:54 -0600

    Added order parameters to users list endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,7 +2,20 @@ import db from "../database/database"
 const sqlz = require("sequelize").Sequelize
 const user = require("../database/models/users")(db)
 
-export async function getUsersList(): Promise<{ successful: boolean; userList: object[] | undefined; }> {
+function getOrder(order: string, by: string) {
+    switch (order) {
+        case "name": return [
+            ["first_name", by],
+            ["last_name", by]
+        ]
+        case "salary": return [
+            ["salary", by]
+        ]
+        default: return []
+    }
+}
+
+export async function getUsersList(order: string, by: string): Promise<{ successful: boolean; userList: object[] | undefined; }> {
     let userList;
 
     try {
@@ -16,7 +29,8 @@ export async function getUsersList(): Promise<{ successful: boolean; userList: o
                 [sqlz.json("business_unit.business_unit_ids"), "business_units"],
                 "on_leave",
                 "salary"
-            ]
+            ],
+            order: getOrder(order, by)
         })
     } catch {
         return { successful: false, userList: undefined }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,8 +3,27 @@ import { getUsersList } from "../controllers/users";
 
 const router = Router()
 
-router.get("/users", async (_req, res) => {
-    const data = await getUsersList()
+router.get("/users", async (req, res) => {
+    const { order, by } = req.query
+    const errors: string[] = []
+
+    if (typeof order !== "undefined") {
+        if (typeof order !== "string" || !["name", "salary"].includes(order)) {
+            errors.push("order")
+        }
+    }
+
+    if (typeof by !== "undefined") {
+        if (typeof by !== "string" || !["asc", "desc"].includes(by)) {
+            errors.push("by")
+        }
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({ message: "Invalid parameters", errors })
+    }
+
+    const data = await getUsersList((order as string | undefined) ?? "name", ((by as string | undefined) ?? "asc").toUpperCase())
 
     if (!data.successful) {
         return res.sendStatus(500)
