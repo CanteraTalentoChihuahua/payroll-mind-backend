017a58c19b71d85b20acc815a4f96103259d73b9
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-08 17:12:15 -0600

    Updates /restore status codes and result message

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -52,18 +52,23 @@ router.post("/restore", async (req, res) => {
     const restore = await restorePassword(token, newPassword);
 
     if (!restore.isSuccessful) {
-        return res.status(500).send("Unable to change password. Try again later...");
+        return res.status(500).json({ message: "Unable to change password. Try again later..." });
     }
 
     if (!restore.result) {
-        return res.status(403).send("Unable to change password.");
+        return res.status(403).json({ message: "Unable to change password. Invalid credentials." });
     }
 
-    return res.status(200).send("Password changed correctly.");
+    return res.status(200).json({ message: "Password changed correctly." });
 });
 
 router.get("/privileges", async (req, res) => {
-    res.status(200).send(Privileges);
+    try {
+        res.status(200).send(Privileges);
+    } catch (error) {
+        res.status(503).json({ message: "Unable to send privileges." });
+    }
+
 });
 
 export default router;
