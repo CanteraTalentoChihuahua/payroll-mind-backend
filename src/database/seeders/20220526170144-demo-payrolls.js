decc761742c776d935c02330624d10e3145b90ab
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-09 20:14:18 -0600

    Fixes id bug

--- src/database/seeders/20220526170144-demo-payrolls.js
+++ src/database/seeders/20220526170144-demo-payrolls.js
@@ -1,16 +1,16 @@
 "use strict";
 
 module.exports = {
-    async up (queryInterface, Sequelize) {
+    async up(queryInterface, Sequelize) {
         await queryInterface.bulkInsert("payrolls", [{
             payment_date: "2022-06-01",
             payment_period_id: 1,
-            business_unit_id: 2,
+            business_unit_id: 1,
             total_amount: 12354.23
         },]);
     },
 
-    async down (queryInterface, Sequelize) {
+    async down(queryInterface, Sequelize) {
         await queryInterface.bulkDelete("payrolls", null, {});
     }
 };
