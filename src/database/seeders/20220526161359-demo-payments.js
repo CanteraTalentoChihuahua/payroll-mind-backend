4678da5ffe7d6567c80f91aafdedf76b3742f190
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-13 12:16:46 -0600

    Fixes seeder bug

--- src/database/seeders/20220526161359-demo-payments.js
+++ src/database/seeders/20220526161359-demo-payments.js
@@ -8,7 +8,7 @@ module.exports = {
             automated_bonuses: {automated_bonuses:[456,123]},
             manual_bonuses: {manual_bonuses:[456,123]},
             substracted_amount:1022.36,
-            payment_period_id:3,
+            payment_period_id:1,
             payment_date_id:4,
         }],{}, {automated_bonuses:{type:new Sequelize.JSON()},
             manual_bonuses:{type:new Sequelize.JSON()}
