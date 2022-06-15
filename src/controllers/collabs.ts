f1e776cfdc0efb4ff993f021e1857891d699e35d
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-15 16:11:09 -0600

    basic query

--- src/controllers/collabs.ts
+++ src/controllers/collabs.ts
@@ -5,8 +5,6 @@ const sqlz = require("sequelize").Sequelize;
 const users = require("../database/models/users")(db);
 
 export async function getUsers(businessUnitId: [number]) {
-    console.log(" ");
-
     const userInfo = await users.findAll({
         where: {
             meta: {
