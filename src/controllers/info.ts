61e27d9d6f201611efcc928eb48f0d00d7dbe939
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-22 15:35:54 -0600

    Adds roles info route

--- src/controllers/info.ts
+++ src/controllers/info.ts
@@ -1,3 +1,29 @@
 import db from "../database/database";
+const roles = require("../database/models/roles")(db);
+const businessUnits = require("../database/models/payments_periods")(db);
 
+export async function getPaymentPeriods() {
+    let businessUnitData;
 
+    try {
+        businessUnitData = await businessUnits.findAll();
+
+    } catch (error) {
+        return false;
+    }
+
+    return businessUnitData;
+}
+
+export async function getRoles() {
+    let rolesData;
+
+    try {
+        rolesData = await roles.findAll();
+
+    } catch (error) {
+        return false;
+    }
+
+    return rolesData;
+}
--- src/routes/info.ts
+++ src/routes/info.ts
@@ -1,4 +1,4 @@
-import { getPaymentPeriods } from "../controllers/users";
+import { getPaymentPeriods, getRoles } from "../controllers/info";
 import { Privileges } from "../util/objects";
 import express from "express";
 
@@ -18,11 +18,21 @@ router.get("/privileges", async (req, res) => {
 router.get("/paymentperiods", async (req, res) => {
     const businessUnitData = await getPaymentPeriods();
 
-    if (businessUnitData === false) {
+    if (!businessUnitData) {
         return res.status(400).send("Invalid request.");
     }
 
     return res.status(200).send(businessUnitData);
+});
+
+router.get("/roles", async (req, res) => {
+    const rolesData = await getPaymentPeriods();
+
+    if (!rolesData) {
+        return res.status(400).send("Invalid request.");
+    }
+
+    return res.status(200).send(rolesData);
 });
 
 export default router;
