ecea24afe6bc80f002e37a6d28f4144eef73f18a
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-23 10:32:52 -0600

    Fixes column name

--- src/database/migrations/20220623162056-create-outcomes-users.js
+++ src/database/migrations/20220623162056-create-outcomes-users.js
@@ -12,7 +12,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER
       },
-      income_id: {
+      outcome_id: {
         allowNull: false,
         type: Sequelize.INTEGER
       },
