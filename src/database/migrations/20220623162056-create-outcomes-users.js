72289edb5c8693b192fe4fc45ff6db8bec7801f4
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-23 10:26:34 -0600

    Creates incomes_users migration and model

---
+++ src/database/migrations/20220623162056-create-outcomes-users.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('outcomes_users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      income_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      counter: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('outcomes_users');
+  }
+};
