3afd4a9e0d4b01e8b805c7d8555fea1f1a69fd8b
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-30 12:49:29 -0600

    Basic get route

---
+++ src/routes/payroll.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/payroll/:id", async (req, res) => {
+    // Queries user
+    // Gets incomes, outcomes, salaries
+    // Sums them up
+    // Returns payroll
+});
+
