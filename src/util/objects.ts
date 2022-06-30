22c5b7bb306c4af8ba28032ae68b4de0f63549ec
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-06-30 12:33:57 -0600

    Fixes primary key issue

--- src/util/objects.ts
+++ src/util/objects.ts
@@ -1,38 +1,38 @@
 export const Privileges = {
     CREATE_ADMINS: { id: 1, description: "Crear administradores" },
-    READ_ADMINS: { id: 12, description: "Ver todos los administradores" },
-    EDIT_ADMINS: { id: 13, description: "Editar administradores" },
-    DELETE_ADMINS: { id: 11, description: "Borrar administradores" },
-    REACTIVATE_ADMINS: { id: 14, description: "Reactivar administradores inactivos" },
+    READ_ADMINS: { id: 2, description: "Ver todos los administradores" },
+    EDIT_ADMINS: { id: 3, description: "Editar administradores" },
+    DELETE_ADMINS: { id: 4, description: "Borrar administradores" },
+    REACTIVATE_ADMINS: { id: 5, description: "Reactivar administradores inactivos" },
 
-    CREATE_COLLABORATORS: { id: 3, description: "Crear colaboradores" },
-    READ_COLLABORATORS: {id: 26, description:"Ver todos los colaboradores"},
-    EDIT_COLLABORATORS: { id: 4, description: "Editar colaboradores" },
-    DELETE_COLLABORATORS: { id: 5, description: "Borrar colaboradores" },
-    REACTIVATE_COLLABORATORS: { id: 22, description: "Reactivar colaboradores inactivos" },
+    CREATE_COLLABORATORS: { id: 6, description: "Crear colaboradores" },
+    READ_COLLABORATORS: {id: 7, description:"Ver todos los colaboradores"},
+    EDIT_COLLABORATORS: { id: 8, description: "Editar colaboradores" },
+    DELETE_COLLABORATORS: { id: 10, description: "Borrar colaboradores" },
+    REACTIVATE_COLLABORATORS: { id: 11, description: "Reactivar colaboradores inactivos" },
 
-    CREATE_BUSINESS_UNITS: { id: 2, description: "Crear unidades de negocio" },
-    READ_BUSINESS_UNITS: { id: 8, description: "Leer las unidades de negocio" },
-    EDIT_BUSINESS_UNITS: { id: 9, description: "Editar las unidades de negocio" },
-    DELETE_BUSINESS_UNITS: { id: 10, description: "Remover las unidades de negocio" },
-    REACTIVATE_BUSINESS_UNITS: { id: 15, description: "Reactivar unidades de negocio inactivas" },
+    CREATE_BUSINESS_UNITS: { id: 12, description: "Crear unidades de negocio" },
+    READ_BUSINESS_UNITS: { id: 13, description: "Leer las unidades de negocio" },
+    EDIT_BUSINESS_UNITS: { id: 14, description: "Editar las unidades de negocio" },
+    DELETE_BUSINESS_UNITS: { id: 15, description: "Remover las unidades de negocio" },
+    REACTIVATE_BUSINESS_UNITS: { id: 16, description: "Reactivar unidades de negocio inactivas" },
 
-    CREATE_BONUSES: { id: 23, description: "Crear bonos" },
-    READ_BONUSES: { id: 25, description: "Ver todos los bonos existentes" },
-    EDIT_BONUSES: { id: 27, description: "Editar bonos" },
-    DELETE_BONUSES: { id: 24, description: "Borrar bonos" },
-    ASSIGN_BONUSES: { id: 17, description: "Asignar bonos" },
+    CREATE_BONUSES: { id: 17, description: "Crear bonos" },
+    READ_BONUSES: { id: 18, description: "Ver todos los bonos existentes" },
+    EDIT_BONUSES: { id: 19, description: "Editar bonos" },
+    DELETE_BONUSES: { id: 20, description: "Borrar bonos" },
+    ASSIGN_BONUSES: { id: 21, description: "Asignar bonos" },
 
-    GIVE_PRIVILEGES: { id: 18, description: "Asignar privilegios" },
-    REMOVE_PRIVILEGES: { id: 19, description: "Remover privilegios" },
+    GIVE_PRIVILEGES: { id: 22, description: "Asignar privilegios" },
+    REMOVE_PRIVILEGES: { id: 23, description: "Remover privilegios" },
 
-    CREATE_PAYROLL_RECEIPTS: { id: 6, description: "Generar recibos de nómina" },
-    EDIT_PAYROLL: { id: 7, description: "Editar datos de nómina" },
+    CREATE_PAYROLL_RECEIPTS: { id: 24, description: "Generar recibos de nómina" },
+    EDIT_PAYROLL: { id: 25, description: "Editar datos de nómina" },
 
-    CREATE_REPORTS: { id: 21, description: "Crear reportes" },
-    READ_REPORTS: { id: 16, description: "Ver reportes" },
+    CREATE_REPORTS: { id: 26, description: "Crear reportes" },
+    READ_REPORTS: { id: 27, description: "Ver reportes" },
 
-    READ_USERS: { id: 11, description: "Leer los usuarios del sistema " }            // Specify business unit?
+    READ_USERS: { id: 28, description: "Leer los usuarios del sistema " }            // Specify business unit?
 };
 
 export type Privilege = { id: number, description: string }
