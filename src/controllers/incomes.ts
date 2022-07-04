e0a3bfe1f1f3d986a87a64bb8180c6161182b1d7
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-07-03 22:57:50 -0600

    Adds createUserOutcome and createOutcome

--- src/controllers/incomes.ts
+++ src/controllers/incomes.ts
@@ -2,16 +2,32 @@ import db from "../database/database";
 import { newIncomeData, newOutcomeData } from "../util/objects";
 
 const incomes_users = require("../database/models/incomes_users")(db);
+const outcomes_users = require("../database/models/incomes_users")(db);
 const incomes = require("../database/models/incomes")(db);
+const outcomes = require("../database/models/incomes")(db);
 
-interface incomeObj {
+interface entryObj {
     name: string, automatic: boolean, active: boolean
 }
 
-export async function createIncome(incomeData: incomeObj) {
+export async function createIncome(incomeData: entryObj) {
     try {
         await incomes.create({
             ...incomeData,
+            createdAt: new Date()
+        });
+
+    } catch (error) {
+        return { successful: false };
+    }
+
+    return { successful: true };
+}
+
+export async function createOutcome(outcomeData: entryObj) {
+    try {
+        await incomes.create({
+            ...outcomeData,
             createdAt: new Date()
         });
 
@@ -58,6 +74,52 @@ export async function createUserIncome(userId: number, incomeUserData: newIncome
         await incomes_users.create({
             user_id: userId,
             ...incomeUserData,
+            createdAt: new Date(),
+            updatedAt: null
+        });
+
+        return { successful: true }
+    }
+
+    return { successful: true, updated: true };
+}
+
+export async function createUserOutcome(userId: number, outcomeUserData: newOutcomeData) {
+    let outcomesData;
+
+    // Check if income exists
+    try {
+        outcomesData = await outcomes.findOne({
+            attributes: ["name", "automatic"],
+            where: {
+                id: outcomeUserData.outcome_id,
+                active: true
+            },
+            raw: true
+        })
+
+        if (!outcomesData) {
+            return { successful: false, found: false };
+        }
+
+    } catch (error) {
+        return { successful: false };
+    }
+
+    // Income entry exist, check if incomeUsers exist
+    const updateObj = { counter: outcomeUserData.counter, amount: outcomeUserData.amount }
+    const entryResult = await outcomes_users.update(updateObj, {
+        where: {
+            outcome_id: outcomeUserData.outcome_id,
+            user_id: userId
+        }
+    });
+
+    // Does not exists, create incomeUsers entry
+    if (entryResult[0] === 0) {
+        await outcomes_users.create({
+            user_id: userId,
+            ...outcomeUserData,
             createdAt: new Date(),
             updatedAt: null
         });
