e49e55576b6b45b7e393c3edc0d75a08057af4c3
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-07-03 22:19:21 -0600

    Refactores /incomes/id route

--- src/routes/payroll.ts
+++ src/routes/payroll.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { createIncome, createUserIncome, getIncomesLength } from "../controllers/incomes";
 import { getUserData, getSalary, getIncomes, getOutcomes, calculatePayroll } from "../controllers/payroll";
 
 const router = express.Router();
 
 // privileges(Privileges.READ_REPORTS)
-router.get("/payroll/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
     const { id } = req.params;
 
     // Query user and check activity
@@ -49,6 +50,66 @@ router.get("/payroll/:id", async (req, res) => {
     };
 
     res.status(200).send(payrollObject);
+});
+
+// Create -- missing asign to user id
+// {
+//     "outcome_id": 1,
+//     "counter": 1,
+//     "amount": "17500",
+//     "name": "Fondo de reconstrucción facial",
+//     "automatic": false
+// }
+
+// Create via association
+
+// Dropdown con incomes
+router.post("/incomes/:id", async (req, res) => {
+    // Income_id is optional...
+    let { income_id } = req.body;
+    const { counter, amount, name, automatic } = req.body;
+    const { id } = req.params;
+
+    if (!name && !income_id) {
+        return res.status(400).json({ message: "Missing either name or income_id." });
+    }
+
+    if (name && income_id) {
+        return res.status(400).json({ message: "Must not provide name and income_id simultaneously." });
+    }
+
+    // Required
+    if (!counter || !amount || !automatic) {
+        return res.status(400).json({ message: "Missing parameters." });
+    }
+
+    // Income entry does not exist, create it AND RETURN ITS ID
+    if (name) {
+        const newIncomeData = await createIncome({ name, automatic, active: true });
+
+        if (!newIncomeData.successful) {
+            return res.status(400).json({ message: "Invalid request. Entry might be duplicate." });
+        }
+
+        income_id = await getIncomesLength();
+    }
+
+    const newIncomeData = await createUserIncome(parseInt(id), { income_id, counter, amount, automatic });
+
+    if (!newIncomeData.successful) {
+        return res.sendStatus(500);
+    }
+
+    if (newIncomeData.updated) {
+        return res.status(200).json({ message: "Income was updated successfully." })
+    }
+
+    return res.status(200).json({ message: "Income was registered successfully." });
+});
+
+
+router.post("/trial", async (req, res) => {
+    getIncomesLength();
 });
 
 export default router;
