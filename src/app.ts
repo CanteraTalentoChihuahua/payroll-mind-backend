c5191af7dd63b3f63d4e6e34ed39799578849b41
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-07-05 10:18:36 -0600

    Modify cors

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(cors());
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
