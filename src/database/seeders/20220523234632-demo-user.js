3675d729057328a75dc5179d4f74dc02e7b31299
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-07-05 17:16:45 -0600

    Removes privileges from admin

--- src/database/seeders/20220523234632-demo-user.js
+++ src/database/seeders/20220523234632-demo-user.js
@@ -41,7 +41,7 @@ module.exports = {
                 phone_number: "6622274328",
                 role_id: 2,
                 privileges: {
-                    privileges: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+                    privileges: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
                 },
                 payment_period_id: 1,
                 business_unit: {
