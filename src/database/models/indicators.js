a8a573077c3e3d36feb11bb219a3611a67afa33d
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-07-31 11:14:18 -0600

    changed name of model to indicators

--- src/database/models/indicators.js
+++ src/database/models/indicators.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class metrics extends Model {
+  class indicators extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  metrics.init({
+  indicators.init({
     name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'metrics',
+    modelName: 'indicators',
     paranoid: true
   });
-  return metrics;
+  return indicators;
 };
