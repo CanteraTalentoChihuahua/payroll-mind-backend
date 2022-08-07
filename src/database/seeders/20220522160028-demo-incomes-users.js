42ba4156ab964338692ce3e90f4b1fe735661792
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-08-07 15:25:39 -0600

    updates seeder

--- src/database/seeders/20220522160028-demo-incomes-users.js
+++ src/database/seeders/20220522160028-demo-incomes-users.js
@@ -1,8 +1,50 @@
+/* eslint-disable indent */
 'use strict';
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert("incomes_users", [
+      // User: 2 - current - 08/15/2022
+      {
+        user_id: 2,
+        income_id: 1,
+        counter: 1,
+        amount: 2300
+      },
+      {
+        user_id: 2,
+        income_id: 2,
+        counter: 1,
+        amount: 7000
+      },
+      {
+        user_id: 2,
+        income_id: 4,
+        counter: 2,
+        amount: 500
+      },
+
+      // User: 2 - past - 07/15/2022
+      {
+        user_id: 2,
+        income_id: 1,
+        counter: 1,
+        amount: 2300
+      },
+      {
+        user_id: 2,
+        income_id: 2,
+        counter: 1,
+        amount: 7000
+      },
+      {
+        user_id: 2,
+        income_id: 4,
+        counter: 2,
+        amount: 500
+      },
+
+      // User: 2 - past - 06/15/2022
       {
         user_id: 2,
         income_id: 1,
